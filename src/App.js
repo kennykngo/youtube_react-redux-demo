7461df5e25a21e7f8338e0d01062643fbd5de635
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-04-09 14:22:38 -0700

    created ItemContainer component and imported it then applied the ownProps to both of the connect methods from react-redux

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import MuffinContainer from './components/MuffinContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
+        <ItemContainer cake />
+        <ItemContainer cak />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
